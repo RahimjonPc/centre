e1d29966b91d38dd55719979ea36668cb955be5e
RahimjonPc <abduganievrahimjon@gmail.com>
2021-03-24 15:30:17 +0500

    urls edinstvennoe chislo

--- api/v1/urls.py
+++ api/v1/urls.py
@@ -3,8 +3,8 @@ from django.urls import path, include
 app_name = 'api_v1'
 
 urlpatterns = [
-    path('cources/', include('api.v1.cources.urls')),
-    path('events/', include('api.v1.events.urls')),
-    path('marks/', include('api.v1.marks.urls')),
-    path('users/', include('api.v1.users.urls')),
+    path('cource/', include('api.v1.cources.urls')),
+    path('event/', include('api.v1.events.urls')),
+    path('mark/', include('api.v1.marks.urls')),
+    path('user/', include('api.v1.users.urls')),
 ]
