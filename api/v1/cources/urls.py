1e0a9de1e012533943336da524d96183e289832e
RahimjonPc <abduganievrahimjon@gmail.com>
2021-03-24 15:28:59 +0500

    urls edinstvennoe chislo

--- api/v1/cources/urls.py
+++ api/v1/cources/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import CoursesListView, CoursesDetail
 
 urlpatterns = [
-    path('cources/list/', CoursesListView.as_view(), name='course_list'),
-    path('cources/detail/<int:pk>', CoursesDetail.as_view(), name='cources_detail'),
+    path('cource/list/', CoursesListView.as_view(), name='course_list'),
+    path('cource/detail/<int:pk>', CoursesDetail.as_view(), name='cources_detail'),
 ]
--- api/v1/events/urls.py
+++ api/v1/events/urls.py
@@ -2,6 +2,6 @@ from django.urls import path, include
 from .views import EventsListView, EventsDetailView
 
 urlpatterns = [
-    path('events/list/', EventsListView.as_view(), name='events_list'),
+    path('event/list/', EventsListView.as_view(), name='events_list'),
     path('event/detail/<int:pk>', EventsDetailView.as_view(), name='event_detail'),
 ]
