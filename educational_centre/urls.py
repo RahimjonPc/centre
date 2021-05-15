7521e2a0ea7cc4d52cec18d78276c94ef1a38689
RahimjonPc <abduganievrahimjon@gmail.com>
2021-05-15 14:50:14 +0500

    smth

--- educational_centre/urls.py
+++ educational_centre/urls.py
@@ -23,6 +23,9 @@ from rest_framework_simplejwt.views import (
 )
 from . import views
 
+from django.views.static import serve
+from django.conf.urls import url
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
@@ -38,4 +41,7 @@ urlpatterns = [
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+
+    path('admin/', admin.site.urls),
+    path('', include("Home.urls")),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
